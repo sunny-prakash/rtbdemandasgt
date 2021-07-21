1e6f8636865176ef8e9f62cc6e59b99cd0af3b53
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-07-21 15:46:57 +0530

    ajax call

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-let adBlock = document.getElementById("advertise_block");
-let animBlock = document.getElementById("sticky_anim");
+const adBlock = document.getElementById("advertise_block");
+const animBlock = document.getElementById("sticky_anim");
+const addList1 = document.getElementById("adList1");
+const addList2 = document.getElementById("adList2");
+const addList3 = document.getElementById("adList3");
+
+const xhttp = new XMLHttpRequest();
 
 function closeAd(elemId) {
     if (elemId === "advertise_block") {
@@ -8,3 +13,13 @@ function closeAd(elemId) {
         animBlock.style.display = "none";
     }
 }
+xhttp.onload = function () {
+    let data = JSON.parse(xhttp.responseText);
+    addList1.innerHTML = `${data[0].id}<br/>${data[0].username}`;
+    addList2.innerHTML = `${data[1].id}<br/>${data[1].username}`;
+    addList3.innerHTML = `${data[2].id}<br/>${data[2].username}`;
+};
+
+xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+xhttp.send();
+// 'https://jsonplaceholder.typicode.com/users'
