536db8b066969801babe5e047a7eba4b6aea500d
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-07-21 13:19:57 +0530

    complete page

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+let adBlock = document.getElementById("advertise_block");
+let animBlock = document.getElementById("sticky_anim");
 
+function closeAd(elemId) {
+    if (elemId === "advertise_block") {
+        adBlock.style.display = "none";
+    } else if (elemId === "sticky_anim") {
+        animBlock.style.display = "none";
+    }
+}
